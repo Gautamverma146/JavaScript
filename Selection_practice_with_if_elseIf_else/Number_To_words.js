6f22b13a52f722e522c927815befb657c92f8b5e
Gautamverma2003 <gautam146official@gmail.com>
2025-03-05 15:50:48 +0530

    [Gautam] add.Added Read single digit no. and give words

---
+++ Selection_practice_with_if_elseIf_else/Number_To_words.js
@@ -0,0 +1,15 @@
+let number = Number(process.argv[2]);
+
+if(number === 0){ console.log("Zero")}
+else if( number ===1) { console.log("one")}
+else if( number === 2) {console.log("two")}
+else if ( number === 3) {console.log("three")}
+else if (number ===4) { console.log("four")}
+else if (number === 5) console.log("Five");
+else if (number === 6) console.log("Six");
+else if (number === 7) console.log("Seven");
+else if (number === 8) console.log("Eight");
+else if (number === 9) console.log("Nine");
+else {
+    console.log("provide valid number");
+}
